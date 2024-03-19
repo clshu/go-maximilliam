45032ad2052e7c3fc3c560294f8b21a647877fe7
clshu <clshu2020@gmail.com>
2024-03-19 16:30:36 +0800

    Create an investment calculator. Use fmt.Scan and different 'var' or no 'var' scenarios.

---
+++ investment_calculator/investment_calculator.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const inflationRate = 6.5
+	var investmenAmount float64
+	expectedReturnRate := 5.5
+	var years float64
+
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmenAmount)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Investment Years: ")
+	fmt.Scan(&years)
+
+	futureValue := investmenAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
+}
