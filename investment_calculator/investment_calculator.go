eb71d56d78fa11a1eee0e0004b179ae3f8830891
clshu <clshu2020@gmail.com>
2024-03-19 16:58:56 +0800

    Use fmt.Sprintf to create formatted strings.

--- investment_calculator/investment_calculator.go
+++ investment_calculator/investment_calculator.go
@@ -24,6 +24,11 @@ func main() {
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 	// fmt.Println("Future Value: ", futureValue)
 	// fmt.Println("Future Value(adjusted for inflation): ", futureRealValue)
-	fmt.Printf("Future Value: %.2f\n", futureValue)
-	fmt.Printf("Future Value(adjusted for inflation): %.2f\n", futureRealValue)
+	// fmt.Printf("Future Value: %.2f\n", futureValue)
+	// fmt.Printf("Future Value(adjusted for inflation): %.2f\n", futureRealValue)
+
+	formattedFV := fmt.Sprintf("Future Value: %.2f\n", futureValue)
+	fotmattedRFV := fmt.Sprintf("Future Value(adjusted for inflation): %.2f\n", futureRealValue)
+
+	fmt.Print(formattedFV, fotmattedRFV)
 }
