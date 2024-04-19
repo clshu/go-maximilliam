f5d914a19374703e1f3ef80355c9929fbe1bb59c
clshu <clshu2020@gmail.com>
2024-04-19 12:02:25 +0800

    36, 37, 38, 39, 40: if, else if, else

---
+++ bank/bank.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var accountBalance = 1000.0
+
+	fmt.Println("Welcome to Go Bank!")
+	fmt.Println("What do you want to do?")
+	fmt.Println("1. Check balance")
+	fmt.Println("2. Deposit")
+	fmt.Println("3. Withdraw money")
+	fmt.Println("4. Exit")
+
+	var choice int
+	fmt.Print("Your choice: ")
+	fmt.Scan(&choice)
+
+	if choice == 1 {
+		fmt.Println("Balance:", accountBalance)
+	}
+
+	fmt.Println("Your choice:", choice)
+
+}
