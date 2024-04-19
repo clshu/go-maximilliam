4315f27180b36e78a91c4774814faa61e7cb9f7d
clshu <clshu2020@gmail.com>
2024-04-19 12:06:10 +0800

    41, 42, 43,44, 45: Infinite Loop 'for', 'break', 'continue', 'return', 'switch', 'switch and break'

--- bank/bank.go
+++ bank/bank.go
@@ -5,23 +5,47 @@ import (
 )
 
 func main() {
-	var accountBalance = 1000.0
+	var accountBalance float64 = 1000.0
 
-	fmt.Println("Welcome to Go Bank!")
-	fmt.Println("What do you want to do?")
-	fmt.Println("1. Check balance")
-	fmt.Println("2. Deposit")
-	fmt.Println("3. Withdraw money")
-	fmt.Println("4. Exit")
+	for {
+		fmt.Println("\nWhat do you want to do?")
+		fmt.Println("1. Check balance")
+		fmt.Println("2. Deposit")
+		fmt.Println("3. Withdraw money")
+		fmt.Println("4. Exit")
 
-	var choice int
-	fmt.Print("Your choice: ")
-	fmt.Scan(&choice)
+		var choice int
+		fmt.Print("Your choice: ")
+		fmt.Scan(&choice)
 
-	if choice == 1 {
-		fmt.Println("Balance:", accountBalance)
+		switch choice {
+		case 1:
+			fmt.Println("Balance:", accountBalance)
+		case 2:
+			fmt.Print("Your deposit: ")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
+			accountBalance += depositAmount
+			fmt.Println("Balance updated! New balance: ", accountBalance)
+		case 3:
+			fmt.Print("Withdrawal Amount: ")
+			var withdrawalAmount float64
+			fmt.Scan(&withdrawalAmount)
+
+			if accountBalance < withdrawalAmount {
+				fmt.Println("Not sufficient fund. Current balance: ", accountBalance)
+				continue
+			}
+			accountBalance -= withdrawalAmount
+			fmt.Println("Balance updated! New balance: ", accountBalance)
+		case 4:
+			fmt.Println("Goodbye!")
+			fmt.Println("Thanks for choosing our bank!")
+			return
+		default:
+			fmt.Println("Invalid choice.")
+		}
+
 	}
-
-	fmt.Println("Your choice:", choice)
 
 }
