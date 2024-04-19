e2e6ac286617a1f94a26f934d3f4aadc297f8ef7
clshu <clshu2020@gmail.com>
2024-04-19 22:48:27 +0800

    50. Section Exercise - The Task

---
+++ exercise-3-starting-snapshot/profit_calculator.go
@@ -0,0 +1,36 @@
+package main
+
+// Goals
+// 1) Validate user input
+//	=> Show error message & exit if invalid input is provided
+// 	- No negative numbers
+// 	- No 0
+// 2) Store calculated results into file
+
+import "fmt"
+
+func main() {
+	revenue := getUserInput("Revenue: ")
+	expenses := getUserInput("Expenses: ")
+	taxRate := getUserInput("Tax Rate: ")
+
+	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
+
+	fmt.Printf("%.1f\n", ebt)
+	fmt.Printf("%.1f\n", profit)
+	fmt.Printf("%.3f\n", ratio)
+}
+
+func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	ebt := revenue - expenses
+	profit := ebt * (1 - taxRate/100)
+	ratio := ebt / profit
+	return ebt, profit, ratio
+}
+
+func getUserInput(infoText string) float64 {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+	return userInput
+}
